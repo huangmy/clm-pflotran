d762ce0776d9934a04fc198630c1e1d2006f79fb
Gautam Bisht <bishtg@ornl.gov>
2011-11-23 08:18:06 -0500

    Generalized the script to use environmental variable $CLM_COUPLED_MODEL

--- scripts/coupled_model_templates/check_clm.sh
+++ scripts/coupled_model_templates/check_clm.sh
@@ -5,7 +5,7 @@ MODEL=$1
 if [ $MODEL = "clm" ]
 then
     echo "This is CLM model"
-    cp -f /ccs/home/gb9/COUPLED_MODEL_TEMPLATES/Srcfiles.CLM_PFLOTRAN Srcfiles
-    cp -f /ccs/home/gb9/COUPLED_MODEL_TEMPLATES/Depends.CLM_PFLOTRAN Depends
+    cp -f $CLM_COUPLED_MODEL/scripts/coupled_model_templates/Srcfiles.CLM_PFLOTRAN Srcfiles
+    cp -f $CLM_COUPLED_MODEL/scripts/coupled_model_templates/Depends.CLM_PFLOTRAN Depends
 fi
 
