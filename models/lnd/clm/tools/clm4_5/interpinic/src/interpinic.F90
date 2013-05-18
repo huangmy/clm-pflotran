6d8c943fa8e799bfa95ad00607a899e75a8e48b5
Gautam Bisht <bishtg@ornl.gov>
2013-05-17 21:49:44 -0600

    clm4_5_06 tag from NCAR

--- models/lnd/clm/tools/clm4_5/interpinic/src/interpinic.F90
+++ models/lnd/clm/tools/clm4_5/interpinic/src/interpinic.F90
@@ -1486,7 +1486,7 @@ contains
     character(len= 5) :: zone
     character(len=18) :: datetime
     character(len=256):: version = &
-         "$HeadURL: https://svn-ccsm-models.cgd.ucar.edu/clm2/trunk_tags/clm4_5_05/models/lnd/clm/tools/clm4_5/interpinic/src/interpinic.F90 $"
+         "$HeadURL: https://svn-ccsm-models.cgd.ucar.edu/clm2/trunk_tags/clm4_5_06/models/lnd/clm/tools/clm4_5/interpinic/src/interpinic.F90 $"
     character(len=256)  :: revision_id = "$Id: interpinic.F90 47042 2013-05-10 21:39:49Z erik $"
     character(len=16)   :: logname
     character(len=16)   :: hostname
